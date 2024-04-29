c1d820b736f442406e790c13a2cc764b1de58bba
Lullaby330 <69coder@gmail.com>
2024-04-29 09:51:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5862070819:AAHHoUUxfd5pgmBR-WEImbHWg9jJI0cp5Z")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6621046091:AAFU4-CwKXsKJrmsyf1iAnw5dprL9rHWb3U")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "6216349"))
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "6216349"))
 API_HASH = os.environ.get("API_HASH", "5c7418e9f3df6db931caa7354521c55f")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001836874506"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002005495648"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1850233338"))
@@ -29,10 +29,10 @@ PORT = os.environ.get("PORT", "2030")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://makan:makan@cluster0.og6i2.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "nobar")
+DB_NAME = os.environ.get("DATABASE_NAME", "sang")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-100"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001786165494"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -40,7 +40,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nSaya Dapat Menyimpan File Pribadi Dan Pengguna Lain Dapat Mengaksesnya Dari Tautan Khusus.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS",  "5084234580").split()):
+    for x in (os.environ.get("ADMINS",  "5832283378").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -64,7 +64,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌ Jangan Kirimi Saya Pesan Secara Langsung. Saya Hanya Bisa Digunakan Admin channel!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1850233338)
+ADMINS.append(5832283378)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
